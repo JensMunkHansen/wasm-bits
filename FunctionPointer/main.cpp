832d858a7ee598c9ca2ecd69d1157ae7a33ee909
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-06-16 22:01:27 +0200

    ..

--- FunctionPointer/main.cpp
+++ FunctionPointer/main.cpp
@@ -3,17 +3,6 @@
 #include <iostream>
 #include <string>
 
-#if 0
-class_<std::function<std::string(std::string)>>("StringFunctorString")
-        .constructor<>()
-        .function("opcall", &std::function<std::string(std::string)>::operator())
-        ;
-
-
-
-
-#endif
-
 static int runCallback(emscripten::val arg) {
   //  int fp = emscripten::val::module_property("addFunction")(arg, std::string("vi")).as<int>();
   int fp = emscripten::val::module_property("addFunction")(arg, std::string("vi")).as<int>();
@@ -24,19 +13,6 @@ static int runCallback(emscripten::val arg) {
   return 0;
 }
 
-#if 0
-EMSCRIPTEN_DECLARE_VAL_TYPE(CallbackType);
-
-int function_with_callback_param(CallbackType ct) {
-    ct(val("hello"));
-    return 0;
-}
-
-EMSCRIPTEN_BINDINGS(custom_val) {
-  emscripten::function("function_with_callback_param", &function_with_callback_param);
-  emscripten::register_type<CallbackType>("(message: string) => void");
-}
-#endif
 
 EMSCRIPTEN_BINDINGS(fptr_bindings) {
     emscripten::function("runCallback", &runCallback);
