c97f3c1267d481366f1ce34b61240d266c0fe4db
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-06-17 14:33:02 +0200

    StdFunction

---
+++ StdFunction/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/jmh/github/wasm/wasm-bits/StdFunction
+# Build directory: /home/jmh/github/wasm/wasm-bits/StdFunction/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(FunctionPointer_Execute_FunctionCalled "npm" "run" "test")
+set_tests_properties(FunctionPointer_Execute_FunctionCalled PROPERTIES  WORKING_DIRECTORY "/home/jmh/github/wasm/wasm-bits/StdFunction/build" _BACKTRACE_TRIPLES "/home/jmh/github/wasm/wasm-bits/StdFunction/CMakeLists.txt;64;add_test;/home/jmh/github/wasm/wasm-bits/StdFunction/CMakeLists.txt;0;")
