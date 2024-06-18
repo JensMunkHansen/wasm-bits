2ef7c559fd5029a54c04114fea7e21ff6f4e2ed9
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-06-18 23:00:33 +0200

    ..

--- CallbackCommand/build.sh
+++ CallbackCommand/build.sh
@@ -2,7 +2,8 @@
 
 (
     source "$HOME/emsdk/emsdk_env.sh"
-    emcmake cmake -S. -Bbuild -DVTK_DIR=$TSPKG_ROOT/ArtifactoryInstall/Wasm32/Release/lib/cmake/vtk
+    #emcmake cmake -S. -Bbuild -DVTK_DIR=$TSPKG_ROOT/ArtifactoryInstall/Wasm32/Release/lib/cmake/vtk
+    emcmake cmake -S. -Bbuild -DVTK_DIR=/home/jmh/github/VTKCMake/install/lib/cmake/vtk
 #    bear -- bash -c 'cmake --build build --target main'
     cmake --build build --target main
 )
