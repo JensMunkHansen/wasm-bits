9ab9257af433dab0604aae50c0d7c69cd79a4a96
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-07-08 08:38:13 +0200

    ..

--- build.sh
+++ build.sh
@@ -3,10 +3,8 @@
 (
     echo "Emscripten"
     source "$HOME/github/emsdk/emsdk_env.sh"
-    #make
     emcmake cmake -S. -Bbuild -DVTK_DIR=/home/jmh/github/VTKCMake/install/lib/cmake/vtk
     cmake --build build
-    #ctest
 )
 
 
