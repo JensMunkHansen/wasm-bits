37b849d512acb7de6a4fa19a0454050a929424f5
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-07-13 21:12:33 +0200

    removed stuff

--- vtkEmbinding/vtkEmbindUtils.h
+++
@@ -1,5 +0,0 @@
-// Teaches emscripten how to delete vtkObject derived instances.
-#define vtkAddDestructor(cname)                                                \
-  template <> void emscripten::internal::raw_destructor<cname>(cname * ptr) {  \
-    ptr->Delete();                                                             \
-  }
